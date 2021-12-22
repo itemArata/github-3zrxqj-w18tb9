4894642b4de9b6d44f1b908dcddf0d57dac3ea2e
itemArata <arata.hatori@i-tem.info>
2021-12-23 01:59:56 +0900

    1 file modified

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,26 +1,16 @@
-import { Link } from 'react-router-dom';
-import { getInvoices } from '../data';
+import { useParams } from 'react-router-dom';
+import { getInvoice } from '../data';
 
-export default function Invoices() {
-  let invoices = getInvoices();
+export default function Invoice() {
+  let params = useParams();
+  let invoice = getInvoice(parseInt(params.invoiceId, 10));
   return (
-    <div style={{ display: 'flex' }}>
-      <nav
-        style={{
-          borderRight: 'solid 1px',
-          padding: '1rem',
-        }}
-      >
-        {invoices.map((invoice) => (
-          <Link
-            style={{ display: 'block', margin: '1rem 0' }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </Link>
-        ))}
-      </nav>
-    </div>
+    <main style={{ padding: '1rem' }}>
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date: {invoice.due}</p>
+    </main>
   );
 }
